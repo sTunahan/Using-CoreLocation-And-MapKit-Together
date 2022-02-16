1fd24ba354f8151840ca2a61dbdd57c681b2807d
sTunahan <st.ustuntepe@gmail.com>
2022-02-16 15:37:06 +0300

    Initial Commit

---
+++ usingCoreLocation_and_MapKit_together/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  usingCoreLocation_and_MapKit_together
+//
+//  Created by asuftech on 16.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ usingCoreLocation_and_MapKit_togetherUITests/usingCoreLocation_and_MapKit_togetherUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  usingCoreLocation_and_MapKit_togetherUITestsLaunchTests.swift
+//  usingCoreLocation_and_MapKit_togetherUITests
+//
+//  Created by asuftech on 16.02.2022.
+//
+
+import XCTest
+
+class usingCoreLocation_and_MapKit_togetherUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
